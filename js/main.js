f8b877117bed9e8e6fe0ef3c81206496635c1c99
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-24 15:06:53 +0200

    bonus complete

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ createApp({
   data() {
     return {
       content: 'Elden Ring',
+      imgSrc: 'https://p325k7wa.twic.pics/high/elden-ring/elden-ring/00-page-setup/Elden-Ring-KF01.jpg?twic=v1/resize=700/step=10/quality=80'
     }
   }
 }).mount('#app')
